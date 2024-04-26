298f2ed033db85b9a8df78b98988fbe1dec839ed
ropeJIbI4 <pogi199411@gmail.com>
2024-04-26 23:28:47 +0300

    first

---
+++ myapp2/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Clientname: {self.name}, email: {self.email}, phone: {self.phone},address: {self.address}, registration_date: {self.registration_date}'
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    added_date = models.DateTimeField(auto_now_add=True)
+
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, related_name="orders")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    ordered_date = models.DateTimeField(auto_now_add=True)
