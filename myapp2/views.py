8511826e99a5759cb3c8022d03d052bf1e4778f9
ropeJIbI4 <pogi199411@gmail.com>
2024-05-02 22:06:49 +0300

    first commit

--- myapp2/views.py
+++ myapp2/views.py
@@ -5,32 +5,27 @@ from .forms import ProductForm
 
 
 def ordered_products(request):
-    # Получить текущую дату и время
+
     now = datetime.now()
 
-    # Получить заказы за последние 7 дней
     orders_7_days = Order.objects.filter(
         order_date__range=[now - timedelta(days=7), now]
     ).prefetch_related("products")
 
-    # Получить заказы за последние 30 дней
     orders_30_days = Order.objects.filter(
         order_date__range=[now - timedelta(days=30), now]
     ).prefetch_related("products")
 
-    # Получить заказы за последние 365 дней
     orders_365_days = Order.objects.filter(
         order_date__range=[now - timedelta(days=365), now]
     ).prefetch_related("products")
 
-    # Передать переменные контекста в шаблон
     context = {
         "orders_7_days": orders_7_days,
         "orders_30_days": orders_30_days,
         "orders_365_days": orders_365_days,
     }
 
-    # Отрендерить шаблон с переменными контекста
     return render(request, "index.html", context)
 
 
